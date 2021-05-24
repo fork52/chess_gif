49ed1199abee23d2eb57d3f25e181e58aaec7a08
fork52 <mrunankmistry52@gmail.com>
2021-05-24 20:06:50 +0530

    Capitalized constants

--- chess_gif/constants.py
+++ chess_gif/constants.py
@@ -1,17 +1,14 @@
-    
-
-
-yellow_theme =  ( '#ffffdd','#86a666')
+YELLOW_BLUE_THEME =  ( '#ffffdd','#86a666')
 """
 Classic blue - yellow theme.
 """
 
-brown_theme = ('#f0d9b5', '#b58863')
+BROWN_YELLOW_THEME = ('#f0d9b5', '#b58863')
 """
 Classic brown - yellow theme.
 """
 
-ice_theme = ('#dee3e6','#8ca2ad')
+ICE_THEME = ('#dee3e6','#8ca2ad')
 """
 Classic ice theme.
 """
